725fd347488493185010c86dcc9cff8b896eb972
Dat <datdao879@gmail.com>
2025-10-31 11:21:40 +0700

    Test thử sử dụng UI main window dderr làm (Không sử dụng Tkinter)

--- hiengiaodien.py
+++ hiengiaodien.py
@@ -1,25 +1,20 @@
 import sys
-from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QWidget
-from giaodien import *
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from GUItest import Ui_MainWindow
 from xuly import *
-from xuly import socham
-class MainWindow():
-        def __init__(self):
-            self.main_win=QWidget()
-            self.ui=Ui_Form()
-            self.ui.setupUi(self.main_win)
-            self.lienketnutlenh()
-            self.main_win.show()
-        def lienketnutlenh(self):
-            self.ui.pbtgieo.clicked.connect(self.xulinoibo)
-        def xulinoibo(self):
-            ketqua=tungxucxac(socham)
-            self.ui.lneketqua.setText(ketqua)
 
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.btngieo.clicked.connect(self.xu_ly)
+    def xu_ly(self):
+        self.lnketqua.setText(str(tungxucxac(socham)))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     app.exec()
