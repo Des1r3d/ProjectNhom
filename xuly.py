51128811b8cd15d9966e73742dbb08cd09b50d98
Y <yndnk25406@st.uel.edu.vn>
2025-10-31 08:23:21 +0700

    test

--- xuly.py
+++ xuly.py
@@ -1,10 +1,12 @@
+#import hàm random từ python
 import random
 #tạo list gồm các mặt của số chấm
 socham = [1, 2, 3, 4, 5, 6]
 def tungxucxac(socham):
-    #import hàm random từ python
+
 
     #yêu cầu chương trình random các số trong danh sách socham và trả về kết quả
     return(random.choice(socham))
-
 #khi nối giao diện với hàm thì khai báo danh sách socham rieng le rồi import hàm def
+if __name__ == '__main__':
+    print(tungxucxac(socham))
